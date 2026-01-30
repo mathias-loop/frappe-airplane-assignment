f6dae2db775afed88ca37f0a432da271461bd2ee
mathias-loop <mathias@loopservices.mt>
2026-01-30 07:33:07 +0000

    feat: adding total cost web hook and adding flight price and total amount

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -7,3 +7,13 @@ from frappe.model.document import Document
 
 class AirplaneTicket(Document):
 	pass
+
+	def before_save(self):
+		total = self.flight_price or 0.0
+
+		# Add the total cost of all the items. 
+		if self.add_ons:
+			for item in self.add_ons:
+				total += item.amount
+		
+		self.total_amount = total
