ffa50bdf0305c8ffbb1be19d952c76b8189902d4
mathias-loop <mathias@loopservices.mt>
2026-01-30 08:03:23 +0000

    feat: remove duplicate items from add-ons table

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -17,3 +17,18 @@ class AirplaneTicket(Document):
 				total += item.amount
 		
 		self.total_amount = total
+	
+	def validate(self): 
+		# Removes duplicate items from the add-ons table 
+
+		# Creating a checklist to keep track of items we have seen 
+		seen_items = set()
+		unique_rows = [] 
+
+		# Loop through the current add-ons 
+		for row in self.add_ons: 
+			if row.item not in seen_items:
+				unique_rows.append(row)
+				seen_items.add(row.item)
+
+		self.add_ons = unique_rows
