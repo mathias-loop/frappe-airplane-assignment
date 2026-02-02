ea6834e58b2c3d087ea80f1832eb8f342520df5e
mathias-loop <mathias@loopservices.mt>
2026-02-02 08:27:48 +0000

    refactor: enable WebsiteGenerator and fix status field

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -2,10 +2,13 @@
 # For license information, please see license.txt
 
 # import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
 from frappe.model.document import Document
 
 
-class AirplaneFlight(Document):
+
+class AirplaneFlight(WebsiteGenerator):
 	pass
 
 	def on_submit(self): 
