28ae84c9cda2c2e2bc7bea8a7e113fdff6f90db9
mathias-loop <mathias@loopservices.mt>
2026-02-02 12:28:57 +0000

    feat: add website field and visit link button to Airline form

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -6,3 +6,13 @@
 
 // 	},
 // });
+
+frappe.ui.form.on('Airline', {
+    refresh: function(frm) {
+        // Check if the website field has a value
+        if (frm.doc.website) {
+            // Add a custom button to the form
+            frm.add_web_link(frm.doc.website, "Visit Website");
+        }
+    }
+});
