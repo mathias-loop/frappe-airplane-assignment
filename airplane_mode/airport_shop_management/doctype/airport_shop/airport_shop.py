7854e2d022531aa87562641c975b804f608ab0a0
mathias-loop <mathias@loopservices.mt>
2026-02-03 15:31:46 +0000

    fix(rent-receipt): update naming format and make rent read-only

--- airplane_mode/airport_shop_management/doctype/airport_shop/airport_shop.py
+++ airplane_mode/airport_shop_management/doctype/airport_shop/airport_shop.py
@@ -3,7 +3,15 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
 
 class AirportShop(Document):
 	pass
+
+	def before_insert(self): 
+		# Fetch the default rent from Single DocType 'Shop Settings'
+		default_rent = frappe.db.get_single_value('Shop Settings', 'default_rent_amount')
+		
+		# If the user hasn't typed a rent, apply the default
+		if not self.rent_amount and default_rent:
+			self.rent_amount = default_rent
